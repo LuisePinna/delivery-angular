1223b6f8ca116092907c0003f7669bd9b842c1d5
Luise Pinna <luisepinna@gmail.com>
2021-06-26 08:39:21 -0300

    primeiro commit

---
+++ projeto02/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiRestauranteService } from './api-restaurante.service';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  dadosRestaurante = {
+    nomeRestaurante : '',
+    foto : '',
+    descricao : ''
+  };
+
+  constructor(private apiService: ApiRestauranteService) { }
+
+  ngOnInit(): void {
+
+    this.apiService.getDadosRestaurante()
+      .subscribe(
+        (data:any) => {
+          this.dadosRestaurante.nomeRestaurante = data.nome;
+          this.dadosRestaurante.foto = environment.apiUrl + data.foto;
+          this.dadosRestaurante.descricao = data.descricao;
+        },
+        e => {
+          console.log(e);
+        }
+      );
+
+  }
+
+}
---
+++ projeto02/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://apirestaurante3-001-site1.itempurl.com'
+};
